36e4c0c49623d39775f12d176a4fe315e70ab6ba
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-16 12:11:18 +0100

    EighthTask.Logic was changed. Interface ISequenceBuilder was added

---
+++ EighthTask.Logic/Components/Builders/Interfaces/ISequenceBuilder.cs
@@ -0,0 +1,9 @@
+﻿using EighthTask.Logic.Components.Interfaces;
+
+namespace EighthTask.Logic.Components.Builders.Interfaces
+{
+    public interface ISequenceBuilder
+    {
+        ISequence Create();
+    }
+}
