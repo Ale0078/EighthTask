b6c8b3225d9b38cdfe8b61c32eaed419c23746d3
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-16 12:02:23 +0100

    Nlog was added to project

--- EighthTask/Program.cs
+++ EighthTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using NLog;
 
 namespace EighthTask
 {
@@ -6,6 +7,7 @@ namespace EighthTask
     {
         static void Main(string[] args)
         {
+            LogManager.GetCurrentClassLogger().Info("Info");
             Console.WriteLine("Hello World!");
         }
     }
