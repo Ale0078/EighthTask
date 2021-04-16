802937baf86a23c9ebf923a24a9493fb21cd0237
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-16 12:14:19 +0100

    EighthTask.Logic was changed. Class FibonacciSequenceBuilder was added

---
+++ EighthTask.Logic/Components/Builders/FibonacciSequenceBuilder.cs
@@ -0,0 +1,22 @@
+﻿using EighthTask.Logic.Components.Builders.Interfaces;
+using EighthTask.Logic.Components.Interfaces;
+
+namespace EighthTask.Logic.Components.Builders
+{
+    public class FibonacciSequenceBuilder : ISequenceBuilder
+    {
+        public int MaxRange { get; protected set; }
+        public int MinRange { get; protected set; }
+
+        public FibonacciSequenceBuilder(int maxRange, int minRange) 
+        {
+            MaxRange = maxRange;
+            MinRange = minRange;
+        }
+
+        public ISequence Create() 
+        {
+            return new FibonacciSequence(MaxRange, MinRange);
+        }
+    }
+}
