279b2dc5f460db40c3ea08797520a1b08ca09f5b
Dytiuk Oleksii <dytyuk624@gmail.com>
2021-04-16 12:34:47 +0100

    EighthTask was changed. Class FibonacciView was added

---
+++ EighthTask/Views/FibonacciView.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+
+using EighthTask.Logic.UserInterface.Abstracts;
+
+namespace EighthTask.Views
+{
+    public class FibonacciView : View
+    {
+        public FibonacciView(Model viewModel) : base(viewModel) 
+        {
+        }
+
+        public override void Display()
+        {
+            foreach (var number in ViewModel.Sequence)
+            {
+                Write("{0}, ", number);
+            }
+
+            WriteLine();
+        }
+    }
+}
